7ce3f29341c8d504a2c16c43ca9d9f8c6d25a3f8
David <agutierrezp1701@alumno.ipn.mx>
2023-04-17 19:53:00 -0600

    Eliminación de 2 archivos, acomodo y estilo
    Se eliminaron los archivos de boostrap tags.css del root debido a que ya estaba en la carpeta de JS. se dio formato a "crear recetas" y se eliminó el archivo "Crear_receta.html" ya que ya se encontraba el .php

--- Crear_receta.php
+++ Crear_receta.php
@@ -14,7 +14,7 @@ session_start();
   <!--Titulo-->
   <title>Mi Recetario</title>
   <!--Favicon - icono de la pestaña-->
-  <link rel="icon" type="image/x-icon" href="../Desarrollo%20Axel/images/cocinero.png">
+  <link rel="icon" type="image/x-icon" href="images/cocinero.png">
   <!--Bootstrap-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -26,8 +26,8 @@ session_start();
   <link href="style2.css" rel="stylesheet">
 
   <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="../Desarrollo%20Axel/js/Filter.css">
-  <link rel="stylesheet" href="../Desarrollo%20Axel/js/Bootstrap-Tags-Input-bootstrap-tagsinput.css">
+  <link rel="stylesheet" href="js/Filter.css">
+  <link rel="stylesheet" href="js/Bootstrap-Tags-Input-bootstrap-tagsinput.css">
 
 </head>
 
@@ -42,12 +42,12 @@ session_start();
 
     <div class="collapse navbar-collapse" id="navbar-toggler">
       <a class="navbar-brand" href="index.php">
-        <img src="../Desarrollo%20Axel/images/cocina.png" width="50" alt="Logo de la página web">
+        <img src="images/cocina.png" width="50" alt="Logo de la página web">
       </a>
 
       <form class="d-flex" role="search">
         <input class="form-control me-1" type="search" placeholder="Buscar" aria-label="Search" size="40">
-        <a href="#myModal" data-bs-toggle="modal"><img src="../Desarrollo%20Axel/images/buscar.png" width="30" alt="buscador">
+        <a href="#myModal" data-bs-toggle="modal"><img src="images/buscar.png" width="30" alt="buscador">
         </a>
         <!--<button class="btn btn-outline-success" type="submit">Buscar</button>-->
       </form>
@@ -56,12 +56,12 @@ session_start();
 
         <div class="dropdown">
           <button class="btn btn-light dropdown-toggle btn-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="../Desarrollo%20Axel/images/recetario.svg" width="27" alt="mi recetario">
+            <img src="images/recetario.svg" width="27" alt="mi recetario">
             Mi Recetario
           </button>
           <ul class="dropdown-menu me-2 dropdown-menu-info">
             <li><a class="dropdown-item active" href="#">Mis Recetas</a></li>
-            <li><a class="dropdown-item" href="#">Crear Receta</a></li>
+            <li><a class="dropdown-item" href="Crear_receta.php">Crear Receta</a></li>
             <li><a class="dropdown-item" href="#">Something</a></li>
             <li>
               <hr class="dropdown-divider">
@@ -71,7 +71,7 @@ session_start();
         </div>
 
         <button type="button" class="btn btn-light btn-2">
-          <img src="../Desarrollo%20Axel/images/carrito-de-supermercado.png" width="27" alt="carrito de compras">
+          <img src="images/carrito-de-supermercado.png" width="27" alt="carrito de compras">
           Comprar
         </button>
 
@@ -79,9 +79,9 @@ session_start();
         <div class="dropdown">
           <button class="btn btn-light dropdown-toggle btn-3" type="button" data-bs-toggle="dropdown"
                   aria-expanded="true">
-            <img src="../Desarrollo%20Axel/images/user.png" width="27" alt="carrito de compras">
+            <img src="images/user.png" width="27" alt="carrito de compras">
             <?php
-            include "./php/session.php"
+            include "php/session.php"
               ?>
           </button>
           <ul class="dropdown-menu dropdown-menu-info">
@@ -99,111 +99,115 @@ session_start();
 <!--Sección Hero -> (Sección principal) -->
 <section>
   <div class="container" >
-    <!--Formulario-->
-    <h1> Crear Receta </h1>
-    <br>
-  </div>
-</section>
 
-<!--Otra parte -->
-<form method="post">
-<section>
-  <div class="container">
-    <div class="mb-3">
-      <label for="formGroupExampleInput" class="form-label">Nombre de la receta</label>
-      <input type="text" name="nombre" class="form-control" id="formGroupExampleInput" placeholder="Escriba el nombre de su receta">
-    </div>
+      <div class = "card">
+          <div class="card-header">
+           <h1>Creación de receta</h1>
+          </div>
+          <form method="post">
+              <section>
+                  <div class="container">
+                      <div class="mb-3">
+                          <label for="formGroupExampleInput" class="form-label">Nombre de la receta</label>
+                          <input type="text" name="nombre" class="form-control" id="formGroupExampleInput" placeholder="Escriba el nombre de su receta">
+                      </div>
 
-    <div class="mb-3">
-      <label for="form-select" class="form-label">Tipo Alimentacion</label>
-      <select name="talimentacion" class="form-select" aria-label="Default select example" id="form-select">
-        <option>Omnívora</option>
-        <option>Vegana</option>
-        <option>Vegeteriana</option>
-      </select>
-    </div>
-    <div class="mb-3">
-      <label for="form-select" class="form-label">Categoria</label>
-      <select name="categoria" class="form-select" aria-label="Default select example" id="form-select">
-        <option>Dulce</option>
-        <option>Salado</option>
-      </select>
-    </div>
-</section>
+                      <div class="mb-3">
+                          <label for="form-select" class="form-label">Tipo Alimentacion</label>
+                          <select name="talimentacion" class="form-select" aria-label="Default select example" id="form-select">
+                              <option>Omnívora</option>
+                              <option>Vegana</option>
+                              <option>Vegeteriana</option>
+                          </select>
+                      </div>
+                      <div class="mb-3">
+                          <label for="form-select" class="form-label">Categoria</label>
+                          <select name="categoria" class="form-select" aria-label="Default select example" id="form-select">
+                              <option>Dulce</option>
+                              <option>Salado</option>
+                          </select>
+                      </div>
+              </section>
 
-<!--Otra parte -->
-<br>
-<section>
-  <div class="container">
-    <div class="row g-3">
-      <div class="col">
-        <label for="input_time" class="form-label">Region</label>
-        <input type="text" name="region" class="form-control" placeholder="Pais" aria-label="First name" id="input_time">
+              <!--Otra parte -->
+              <br>
+              <section>
+                  <div class="container">
+                      <div class="row g-3">
+                          <div class="col">
+                              <label for="input_time" class="form-label">Region</label>
+                              <input type="text" name="region" class="form-control" placeholder="Pais" aria-label="First name" id="input_time">
+                          </div>
+                          <div class="col">
+                              <label for="input_porciones" class="form-label">Rendimiento</label>
+                              <input type="text" class="form-control" name="porciones" placeholder="porciones" aria-label="Last name" id="input_porciones">
+                          </div>
+                      </div>
+                  </div>
+              </section>
+
+              <!-- Ingredientes -->
+              <br>
+              <br>
+              <section class="centrado">
+                  <div class="container3 " >
+
+                      <div class="wrap">
+                          <h2>Ingredientes</h2>
+                          <a href="#" class="add">
+                              &plus;
+                          </a>
+                      </div>
+
+                      <div class="inp-ggroup">
+
+                      </div>
+                  </div>
+              </section>
+
+              <!-- Crear pasos de la receta -->
+              <br>
+              <br>
+              <section>
+                  <div class="container">
+                      <h2>Preparación</h2>
+                      <p>
+                          Escribe los pasos de preparación por renglón. <br>Trata de explicar y
+                          detallar cada paso para ser lo mas claro posible, puedes anadir el
+                          tiempo de cocción, congelación, refrigeración u horneado, no olvides
+                          también indicar la temperatura del horno.
+                      </p>
+                      <div class="mb-3">
+                          <label for="exampleFormControlTextarea1" class="form-label">Detalle la preparación de su platillo</label>
+                          <textarea name="preparacion" class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                    placeholder="Escriba los pasos de su receta"></textarea>
+                      </div>
+
+                  </div>
+              </section>
+
+              <br>
+              <br>
+              <section>
+                  <div class="container">
+                      <div class="d-grid gap-2">
+                          <button class="btn btn-success" type="submit">Guardar</button>
+                      </div>
+                  </div>
+              </section>
+              <?php
+              //echo'<p>antes</p>';
+              include "php/cargarReceta.php";
+              ?>
+          </form>
       </div>
-      <div class="col">
-        <label for="input_porciones" class="form-label">Rendimiento</label>
-        <input type="text" class="form-control" name="porciones" placeholder="porciones" aria-label="Last name" id="input_porciones">
-      </div>
-    </div>
-  </div>
-</section>
 
-<!-- Ingredientes -->
-<br>
-<br>
-<section class="centrado">
-  <div class="container3 " >
 
-    <div class="wrap">
-      <h2>Ingredientes</h2>
-      <a href="#" class="add">
-        &plus;
-      </a>
-    </div>
-
-    <div class="inp-ggroup">
-
-    </div>
-  </div>
-</section>
-
-<!-- Crear pasos de la receta -->
-<br>
-<br>
-<section>
-  <div class="container">
-    <h2>Preparación</h2>
-    <p>
-      Escribe los pasos de preparación por renglón. <br>Trata de explicar y
-      detallar cada paso para ser lo mas claro posible, puedes anadir el
-      tiempo de cocción, congelación, refrigeración u horneado, no olvides
-      también indicar la temperatura del horno.
-    </p>
-    <div class="mb-3">
-      <label for="exampleFormControlTextarea1" class="form-label">Detalle la preparación de su platillo</label>
-      <textarea name="preparacion" class="form-control" id="exampleFormControlTextarea1" rows="3"
-                placeholder="Escriba los pasos de su receta"></textarea>
-    </div>
 
   </div>
 </section>
 
-<br>
-<br>
-<section>
-  <div class="container">
-    <div class="d-grid gap-2">
-      <button class="btn btn-success" type="submit">Guardar</button>
-    </div>
-  </div>
-</section>
-<?php
-          //echo'<p>antes</p>';
-          include "./php/cargarReceta.php";
-?>
-</form>
-<br>
-<br>
+
 
 <!-- Scripts -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
@@ -211,7 +215,7 @@ session_start();
         crossorigin="anonymous">
 </script>
 <!-- My script -->
-<script src="../Desarrollo%20Axel/js/create_receta.js"></script>
+<script src="js/create_receta.js"></script>
 
 </body>
 
@@ -221,7 +225,7 @@ session_start();
     <div class="row">
       <!-- LOGO -->
       <a href="index.php" class="col-3 text-reser text-uppercase d-flex align-items-center">
-        <img src="../Desarrollo%20Axel/images/cocina.png" width="75" alt="Logo de la página web" class="img-logo mr-2">
+        <img src="images/cocina.png" width="75" alt="Logo de la página web" class="img-logo mr-2">
         Recetario
       </a>
       <!-- MENÚ -->
@@ -252,6 +256,6 @@ session_start();
   </div>
 </footer>
 
-<script src="../Desarrollo%20Axel/js/Bootstrap-Tags-Input-bootstrap-tagsinput-custom.js"></script>
-<script src="../Desarrollo%20Axel/js/Bootstrap-Tags-Input-bootstrap-tagsinput.min.js"></script>
+<script src="js/Bootstrap-Tags-Input-bootstrap-tagsinput-custom.js"></script>
+<script src="js/Bootstrap-Tags-Input-bootstrap-tagsinput.min.js"></script>
 </html>
