6c97327aaf4808e3ba70cfbb23ba3d5f1a7a912c
Axel7755 <baxeltomas@gmail.com>
2023-05-06 17:52:36 -0600

    log nuevo

---
+++ login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!--Metadatos-->
+  <meta charset="UTF-8">
+  <meta name="ARCM y AT" content="Team: Ultimate">
+  <meta name="description" content="Recetario Ultimate">
+  <meta name="keywords" content="HTML, CSS, JavaScript, ">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--Titulo-->
+  <title>Iniciar sesión</title>
+  <!--Favicon - icono de la pestaña-->
+  <link rel="icon" type="image/x-icon" href="images/cocinero.png">
+  <!--My CSS-->
+  <link href="login.css" rel="stylesheet">
+  <!--Bootstrap-->
+
+  <!-- Google Font Link Icons-->
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+</head>
+
+
+<body>
+
+  <div class="seccion-derecha">
+    <img src="images/cocinero.png" class="img-thumbnail" alt="...">
+
+  </div>
+
+  <div class="box">
+    <span class="borderLine"></span>
+    <form method="post">
+      <h2>Inicia sesión</h2>
+      <div class="inputBox">
+        <input type="text" required="required">
+        <span>Usuario</span>
+        <i></i>
+      </div>
+
+      <div class="inputBox">
+        <input type="password" required="required">
+        <span>Contraseña</span>
+        <i></i>
+      </div>
+      <div class="links">
+        <a href="#">¿Has olvidado tu contraseña?</a>
+        <a href="singn-in.php">Regístrarse</a>
+      </div>
+      <input type="submit" value="Inicia sesión">
+      <?php
+          // echo'<p>antes</p>';
+      include "./php/log.php";
+      ?>
+    </form>
+  </div>
+  <div class="clearfix"></div>
+</body>
+
+</html>
